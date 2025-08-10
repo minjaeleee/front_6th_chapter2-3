32e5700b36e4d1da54b5940dde8c12e58d11f8de
odin <odin@snaps.com>
2025-08-10 15:18:51 +0900

    nvmrc 추가 및 코드 포맷팅 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,21 @@
-import { BrowserRouter as Router } from "react-router-dom"
-import Header from "./components/Header.tsx"
-import Footer from "./components/Footer.tsx"
-import PostsManagerPage from "./pages/PostsManagerPage.tsx"
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import Footer from '@/components/Footer';
+import Header from '@/components/Header';
+import PostsManagerPage from '@/pages/PostsManagerPage';
 
 const App = () => {
   return (
     <Router>
-      <div className="flex flex-col min-h-screen">
+      <div className='flex min-h-screen flex-col'>
         <Header />
-        <main className="flex-grow container mx-auto px-4 py-8">
+        <main className='container mx-auto flex-grow px-4 py-8'>
           <PostsManagerPage />
         </main>
         <Footer />
       </div>
     </Router>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,30 @@
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@/components': path.resolve(__dirname, './src/components'),
+      '@/pages': path.resolve(__dirname, './src/pages'),
+      '@/assets': path.resolve(__dirname, './src/assets'),
+      '@/types': path.resolve(__dirname, './src/types'),
+      '@/utils': path.resolve(__dirname, './src/utils'),
+      '@/hooks': path.resolve(__dirname, './src/hooks'),
+      '@/styles': path.resolve(__dirname, './src/styles'),
+    },
+  },
   server: {
     proxy: {
-      "/api": {
+      '/api': {
         // target: 'https://jsonplaceholder.typicode.com',
-        target: "https://dummyjson.com",
+        target: 'https://dummyjson.com',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
+        rewrite: path => path.replace(/^\/api/, ''),
       },
     },
   },
-})
+});
