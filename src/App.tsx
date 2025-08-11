59462a35cba31ea816cd4d973f9dbaf0c1584b8f
odin <odin@snaps.com>
2025-08-12 00:35:18 +0900

    feat: Shared 분리 (layout ui)

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { BrowserRouter as Router } from 'react-router-dom';
 
-import Footer from '@/components/Footer';
-import Header from '@/components/Header';
 import PostsManagerPage from '@/pages/PostsManagerPage';
+import Footer from '@/shared/ui/Layout/Footer';
+import Header from '@/shared/ui/Layout/Header';
 
 const App = () => {
   return (
