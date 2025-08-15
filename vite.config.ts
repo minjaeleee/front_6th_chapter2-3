051069e97b6d5fc9e3f130d7fcba637422db3e14
odin <odin@snaps.com>
2025-08-15 16:15:07 +0900

    chore: vite config 수정

--- vite.config.ts
+++ vite.config.ts
@@ -2,29 +2,33 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  base: process.env.NODE_ENV === 'production' ? '/front_6th_chapter2-3/' : '/',
+
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
-      '@/components': path.resolve(__dirname, './src/components'),
+      '@/app': path.resolve(__dirname, './src/app'),
       '@/pages': path.resolve(__dirname, './src/pages'),
-      '@/assets': path.resolve(__dirname, './src/assets'),
-      '@/types': path.resolve(__dirname, './src/types'),
-      '@/utils': path.resolve(__dirname, './src/utils'),
-      '@/hooks': path.resolve(__dirname, './src/hooks'),
-      '@/styles': path.resolve(__dirname, './src/styles'),
+      '@/widgets': path.resolve(__dirname, './src/widgets'),
+      '@/features': path.resolve(__dirname, './src/features'),
+      '@/entities': path.resolve(__dirname, './src/entities'),
+      '@/shared': path.resolve(__dirname, './src/shared'),
     },
   },
+
   server: {
-    proxy: {
-      '/api': {
-        // target: 'https://jsonplaceholder.typicode.com',
-        target: 'https://dummyjson.com',
-        changeOrigin: true,
-        rewrite: path => path.replace(/^\/api/, ''),
-      },
-    },
+    proxy:
+      process.env.NODE_ENV === 'development'
+        ? {
+            '/api': {
+              target: 'https://dummyjson.com',
+              changeOrigin: true,
+              rewrite: path => path.replace(/^\/api/, ''),
+            },
+          }
+        : {},
   },
 });
