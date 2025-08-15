40c64da8e9ee8797d799910c38d29cdeab19f99b
odin <odin@snaps.com>
2025-08-15 16:07:55 +0900

    chore: 배포 파일 설정

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,34 @@
-/// <reference types="vitest" />
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vitest/config';
+import path from 'path';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
+
+  base: process.env.NODE_ENV === 'production' ? '/front_6th_chapter2-3/' : '/',
+
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@/app': path.resolve(__dirname, './src/app'),
+      '@/pages': path.resolve(__dirname, './src/pages'),
+      '@/widgets': path.resolve(__dirname, './src/widgets'),
+      '@/features': path.resolve(__dirname, './src/features'),
+      '@/entities': path.resolve(__dirname, './src/entities'),
+      '@/shared': path.resolve(__dirname, './src/shared'),
+    },
+  },
+
+  server: {
+    proxy:
+      process.env.NODE_ENV === 'development'
+        ? {
+            '/api': {
+              target: 'https://dummyjson.com',
+              changeOrigin: true,
+              rewrite: path => path.replace(/^\/api/, ''),
+            },
+          }
+        : {},
   },
 });
